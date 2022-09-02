ebc9e8d015f868f6d4edc17b444fda7393a58b6f
Rusik <noirtxt@gmail.com>
2022-09-02 23:36:22 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
+const sound = new Audio("fx.mp3");
+const sound2 = new Audio("fx2.mp3");
 const button = document.getElementById('btn');
-let blue = '#4A5899';
-let red = '#ED1C24';
+const cont = document.getElementById('container');
+let blue = '#57dfcf';
+let red = '#F42272';
 let flag = 0;
 
 button.addEventListener('click', () => {
     if(flag == 0) { button.style.backgroundColor = red
-    flag++}
+    flag++
+    cont.style.backgroundColor = blue
+    sound.play();
+    }
     else {button.style.backgroundColor = blue
+    cont.style.backgroundColor = red
+    sound2.play();
     flag--};
-    
+
     console.log('sheesh');
 });
